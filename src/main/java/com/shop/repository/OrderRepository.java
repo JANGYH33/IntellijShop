fc74d4555444e2876a12891d36a03def84bf80ba
JANGYH33 <dyd5795@gmail.com>
2021-12-01 22:36:24 +0900

    2021.12.01 SpringBoot SHOP - OrderList - OrderCancel

--- src/main/java/com/shop/repository/OrderRepository.java
+++ src/main/java/com/shop/repository/OrderRepository.java
@@ -1,9 +1,27 @@
 package com.shop.repository;
 
 import com.shop.entity.Order;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
 
 public interface OrderRepository extends JpaRepository<Order, Long> {
+
+
+    // 사용자 주문 데이터
+    @Query("select o from Order o where o.member.email = :email " +
+            "order by o.orderDate desc")
+    List<Order> findOrders(@Param("email") String email, Pageable pageable);
+
+    // 주문갯수 확인
+    @Query("select count(o) from Order o "+
+            "where o.member.email = :email"
+    )
+
+    Long countOrder(@Param("email") String email);
 
 
 }
